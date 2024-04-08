44a8e7a5f934f5ecc9c4f8451ccf559f3351739a
covertjustice <sardarbavneetsingh@gmail.com>
2024-04-08 09:47:52 -0400

    updated readme, added states

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # Searchable Table in ReactJs
 
+**React components** are the building blocks of a React application. To build a React UI, you first break it down into a component hierarchy, then build a static version of the app, and finally add interactivity by managing state.
+The key steps are:
+1. Break the UI into a component hierarchy
+2. Build a static version in React without using state
+3. Identify the minimal but complete representation of UI state
+4. Determine where the state should live in the component hierarchy
+5. Implement two-way data flow between the state and the UI
+
+This is an example of building a searchable product data table, demonstrating how to apply these principles. It it implements topics like one-way data flow, the difference between props and state, and how to pass callbacks from parent to child components to enable updating the state.
--- src/App.js
+++ src/App.js
@@ -1,22 +1,33 @@
+import { useState } from "react";
+
 export default function App() {
   return <FilterableProductTable products={PRODUCTS} />;
 }
 
 function FilterableProductTable({products}) {
+  const [filterText, setFilterText]=useState('');
+  const [inStockOnly, setInStockOnly]=useState(false);
   return (
     <div>
-      <SearchBar />
-      <ProductTable products={products}/>
+      <SearchBar
+        filterText={filterText}
+        inStockOnly={inStockOnly}
+        onFilterTextChange={setFilterText}
+        onInStockOnlyChange={setInStockOnly}/>
+      <ProductTable 
+        products={products}
+        filterText={filterText}
+        inStockOnly={inStockOnly}/>
     </div>
   );
 }
 
-function SearchBar() {
+function SearchBar({filterText, inStockOnly, onFilterTextChange, onInStockOnlyChange}) {
   return(
       <form>
-        <input type="text" placeholder="Search..."/>
+        <input type="text" value={filterText} placeholder="Search..." onChange={(e)=>{ onFilterTextChange(e.target.value)}}/>
         <label>
-          <input type="checkbox"/>
+          <input type="checkbox" checked={inStockOnly} onChange={(e)=>{onInStockOnlyChange(e.target.checked)}}/>
           {" "}
           Only show products in stock
         </label>
@@ -24,11 +35,18 @@ function SearchBar() {
   );
 }
 
-function ProductTable({products}) {
+function ProductTable({products, filterText, inStockOnly}) {
   const rows = [];
   let lastCategory = null;
 
   products.forEach((product) => {
+    if (product.name.toLowerCase().indexOf(filterText.toLowerCase())=== -1) {
+      return;
+    }
+    if (inStockOnly && !product.stocked) {
+      return;
+    }
+
     if (product.category !== lastCategory) {
       rows.push(
         <ProductCategoryRow category={product.category} key={product.category}/>
